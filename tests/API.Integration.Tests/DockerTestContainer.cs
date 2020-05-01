4b9d896ae3b50d296d245a70101ec3aee0159f3c
Anthony Simpson <ajanthon@gmail.com>
2020-05-01 16:26:27 +0100

    First part of getting Docker container working in Integration Test

---
+++ tests/API.Integration.Tests/DockerTestContainer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace PaymentGateway.API.Integration.Tests
+{
+  public class DockerTestContainer
+  {
+    private DockerClient _client;
+    private string ImageName = "eventstore/eventstore";
+    private string ImageTag = "latest";
+    private string ContainerName = "ajs-eventstore-integration-testing";
+
+    public DockerTestContainer()
+    {
+      _client = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine")).CreateClient();
+    }
+    
+    public async Task CreateContainer()
+    {
+      if (await DoesContainerExist(ContainerName))
+      {
+        Console.WriteLine($"Container {ContainerName} exists so deleting it");
+        await DeleteContainer();
+      }
+
+      await _client.Images.CreateImageAsync(new ImagesCreateParameters() {FromImage = ImageName, Tag = ImageTag},
+        new AuthConfig(),
+        new Progress<JSONMessage>());
+
+      var config = new Config() {Hostname = "localhost"};
+
+      var hostConfig = new HostConfig()
+      {
+        PortBindings = new Dictionary<string, IList<PortBinding>>
+        {
+          {"1113/tcp", new List<PortBinding> {new PortBinding {HostIP = "127.0.0.1", HostPort = "1113"}}},
+          {"2113/tcp", new List<PortBinding> {new PortBinding {HostIP = "127.0.0.1", HostPort = "2113"}}}
+        }
+      };
+
+      // Create the container
+      try
+      {
+        CreateContainerResponse response = await _client.Containers.CreateContainerAsync(
+          new CreateContainerParameters(config)
+          {
+            Image = ImageName + ":" + ImageTag, Name = ContainerName, Tty = false, HostConfig = hostConfig,
+          });
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine($"Summat went wrong {e.Message}");
+      }
+    }
+
+    public async Task StartContainer()
+    {
+      bool result = await _client.Containers.StartContainerAsync(ContainerName, new ContainerStartParameters());
+    }
+
+    public async Task DeleteContainer()
+    {
+      await _client.Containers.RemoveContainerAsync(ContainerName, new ContainerRemoveParameters() {Force = true});
+    }
+    
+    private async Task<bool> DoesContainerExist(string label)
+    {
+      IList<ContainerListResponse> containers = await _client.Containers.ListContainersAsync(
+        new ContainersListParameters() {Limit = 10,});
+
+      return containers.Any(c => c.Names.Any(n => n.Contains(label)));
+    }
+  }
+}
--- tests/API.Integration.Tests/PaymentTestFixture.cs
+++ tests/API.Integration.Tests/PaymentTestFixture.cs
@@ -1,20 +1,37 @@
 using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using PaymentGateway.Domain.Interfaces;
 using PaymentGateway.Infrastructure.Persistence.PaymentHistory;
+using Xunit;
 
 namespace PaymentGateway.API.Integration.Tests
 {
-  public class PaymentTestFixture
+  public class PaymentTestFixture : IAsyncLifetime
   {
     public IPaymentHistoryRepository PaymentHistoryRepository { get; }
     public HttpClient TestClient { get; }
+
+    private DockerTestContainer _dockerTestContainer;
 
     public PaymentTestFixture()
     {
       PaymentHistoryRepository = new PaymentHistoryRepository(null, null);
       var appFactory = new WebApplicationFactory<Startup>();
-      TestClient = appFactory.CreateClient();         
+      TestClient = appFactory.CreateClient();
+    }
+
+    public async Task InitializeAsync()
+    {
+      _dockerTestContainer = new DockerTestContainer();
+        
+      await _dockerTestContainer.CreateContainer();
+      await _dockerTestContainer.StartContainer();
+    }
+
+    public async Task DisposeAsync()
+    {
+      await _dockerTestContainer.DeleteContainer();
     }
   }
 }
